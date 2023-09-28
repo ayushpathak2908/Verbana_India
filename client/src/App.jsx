6580c30b05972cbae2eff7b26d4abe28ff78ee1b
ayushpathakk <ayush.pathak@iesoftek.com>
2023-09-29 01:15:11 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Home from "./pages/Home/";
+import Customizer from "./pages/Customizer";
+import Canvas from "./canvas";
+function App() {
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  );
+}
+
+export default App;
